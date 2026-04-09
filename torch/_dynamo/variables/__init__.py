c7b58c0356ef3b64b5d41190ee22345b1d61cae9
Arsh Zahed <arshzahed@meta.com>
2026-04-08 18:13:15 -0700

    [dynamo] Refactor GetAttrBuiltinVariable
    ghstack-source-id: 8f586d11787d85cbe51fd399bf2d6bd214e8e902
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179033

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -21,6 +21,7 @@ from .builtin import (
     BaseBuiltinVariable,
     BuiltinVariable,
     DictBuiltinVariable,
+    GetAttrBuiltinVariable,
     IterBuiltinVariable,
     ListBuiltinVariable,
 )
@@ -211,6 +212,7 @@ __all__ = [
     "DictKeySetVariable",
     "DynamoConfigPatchVariable",
     "FakeItemVariable",
+    "GetAttrBuiltinVariable",
     "GetAttrVariable",
     "GradModeVariable",
     "InspectSignatureVariable",
